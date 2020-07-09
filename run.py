fdf19e249ed93845f794de8fd89e287ce6dc63ec
mkthewlis <mkthewlis@gmail.com>
2020-07-09 11:43:30 +0000

    added chat.html to show user's chat page

--- run.py
+++ run.py
@@ -13,10 +13,6 @@ def add_messages(username, message):
 
     messages.append(messages_dict)
 
-def get_all_messages():
-    """Get all of the messages and separate them with a 'br' tag"""
-    return "<br>".join(messages)
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
@@ -32,7 +28,7 @@ def index():
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return render_template("chat.html", username = username, chat_messages = messages)
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
