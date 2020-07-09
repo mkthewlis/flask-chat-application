1dec3b7fd41a1e7efb5fc91fdb6141d2a7ecac5f
mkthewlis <mkthewlis@gmail.com>
2020-07-09 12:01:28 +0000

    added text box to make chat more user friendly

--- run.py
+++ run.py
@@ -2,17 +2,18 @@ import os
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session
 
-app = Flask (__name__)
+
+app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
+
 def add_messages(username, message):
-    """Add messages to the 'messages' list"""
+    """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages_dict = {"timestamp": now, "from": username, "message": message}
-
     messages.append(messages_dict)
-
+ 
 @app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
@@ -25,15 +26,24 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
-    return render_template("chat.html", username = username, chat_messages = messages)
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
+
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
+
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
